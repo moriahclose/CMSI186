d53d0b538c9f217498b2dbed854d53751457ad2a
mtollive <mtollive@lion.lmu.edu>
2018-01-16 11:35:04 -0800

    Update Greeter.java

--- CMSI186_Projects/homework00/Greeter.java
+++ CMSI186_Projects/homework00/Greeter.java
@@ -1,3 +1,7 @@
+/*****************************************************************************
+Name: Moriah Tolliver         Date: 01/15/18          Purpose: Greet the user
+******************************************************************************/
+
 public class Greeter {
    public static void main( String args[] ) {
       System.out.println( "\n  Hello, world!" );
