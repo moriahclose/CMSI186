4ddce98477a4d7b23f521165bfedf9d8e3caeaff
mtollive <mtollive@lion.lmu.edu>
2018-02-19 14:21:13 -0800

    Add tests to DiceSet.java

--- homework03/DiceSet.java
+++ homework03/DiceSet.java
@@ -1,8 +1,8 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  File name     :  DiceSet.java
  *  Purpose       :  Provides a class describing a set of dice
- *  Author        :  B.J. Johnson
- *  Date          :  2017-02-09
+ *  Author        :  Moriah Tolliver
+ *  Date          :  2018-02-22
  *  Description   :  This class provides everything needed (pretty much) to describe a set of dice.  The
  *                   idea here is to have an implementing class that uses the Die.java class.  Includes
  *                   the following:
@@ -14,6 +14,8 @@
  *                   public String toString();                        // Returns a stringy representation of this set of dice
  *                   public static String toString( DiceSet ds );     // Classwide version of the preceding instance method
  *                   public boolean isIdentical( DiceSet ds );        // Returns true iff this set is identical to the set ds
+ *                   public Die[] getArrayRep();                      // Returns the array of dice as an array instead of a DiceSet
+ *                   public int getSides();                           // Get # of sides on each dice
  *                   public static void main( String[] args );        // The built-in test program for this class
  *
  *  Notes         :  Stolen from Dr. Dorin pretty much verbatim, then modified to show some interesting
@@ -36,6 +38,7 @@ public class DiceSet {
    private int sides;
    private Die[] dice;
    private final int MINIMUM_DICE = 1;
+   private final int MINIMUM_SIDES = 4;
 
    // public constructor:
   /**
@@ -46,14 +49,13 @@ public class DiceSet {
    * @note   parameters are checked for validity; invalid values throw "IllegalArgumentException"
    */
    public DiceSet( int count, int sides ) {
-     count = ( count < MINIMUM_DICE ) ? MINIMUM_DICE : count;
-     dice = new Die[ count ];
-     for (int dieIndex = 0; dieIndex < count; dieIndex++ ) {
-       dice[dieIndex] = new Die( sides );
-
-     }
+     this.count = ( count < MINIMUM_DICE ) ? MINIMUM_DICE : count;
      this.sides = sides;
 
+     dice = new Die[ this.count ];
+     for (int dieIndex = 0; dieIndex < this.count; dieIndex++ ) {
+       dice[dieIndex] = new Die( this.sides );
+     }
    }
 
   /**
@@ -66,6 +68,13 @@ public class DiceSet {
      }
       return sum;
    }
+
+   /**
+    * @return the # of sides on each dice
+    */
+    public int getSides() {
+      return this.sides;
+    }
 
   /**
    * Randomly rolls all of the dice in this set
@@ -91,15 +100,10 @@ public class DiceSet {
   /**
    * Gets the value of an individual die in this set indexed by 'dieIndex'
    * @param  dieIndex int of which die to roll
-   * NOTE: returns 0 if dieIndex is out of range
+   * NOTE: gives error if index is out of range
    */
    public int getIndividual( int dieIndex ) {
-     if (dieIndex >= dice.length) {
-       return 0;
-     }
-     else {
-      return ( dice[ dieIndex ].getValue());
-    }
+      return ( dice[ dieIndex ].getValue() );
    }
 
   /**
@@ -138,24 +142,142 @@ public class DiceSet {
 
   /**
    * @return  tru iff this set is identical to the set passed as an argument
-   * NOTE: identical if dice in both sets have same # of sides and values of dice at the same index are equal
+   * NOTE: identical if same # of dice, samem # of sides, and dice at the same index are equal
    */
-   // needs same amount, same sides, and same values
    public boolean isIdentical( DiceSet ds ) {
-      return this.toString().equals(ds.toString());
+      return this.toString().equals(ds.toString()) && this.getSides() == ds.getSides();
    }
 
   /**
    * A little test main to check things out
    */
    public static void main( String[] args ) {
-      DiceSet myDice = new DiceSet( 1, 4 );
-      DiceSet hisDice = new DiceSet( 1, 4);
-      myDice.roll();
-      hisDice.roll();
-      System.out.println( "My dice: " + myDice.toString() );
-      System.out.println( "His dice: " + myDice.toString( hisDice ) );
-      System.out.println( myDice.isIdentical(hisDice) );
+
+     DiceSet mySet = new DiceSet( 12, 6 );
+     DiceSet theirSet = new DiceSet( 6, 12 );
+
+     // TEST sum()
+     System.out.println("\n6 TESTS FOR sum() [12 6-sided dice]");
+     System.out.print(" Test with no roll (expected output = 0): ");
+     try { System.out.println( mySet.sum()  ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     for (int testVar = 0; testVar < 5; testVar++ ) {
+       System.out.print(" Test with roll " + mySet.toString() + ": ");
+       try { mySet.roll(); System.out.println( mySet.sum()  ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+     }
+
+     // TEST roll()
+     System.out.println("\n6 TESTS FOR roll() [6 12-sided dice]");
+     System.out.println(" Starting set: " + theirSet.toString() );
+
+     for (int testVar = 0; testVar < 6; testVar++ ) {
+       System.out.print(" Next roll: ");
+       try { theirSet.roll(); System.out.println( theirSet.toString() ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+     }
+
+     // TEST rollIndividual()
+     System.out.println("\n20 TESTS FOR rollIndividual()");
+
+     System.out.println(" Starting set [12 6-sided dice]: " + mySet.toString() );
+     for (int testVar = -1; testVar < 13; testVar++ ) {
+       System.out.print(" Roll Dice # " + testVar + ": ");
+       try { mySet.rollIndividual( testVar ); System.out.println( mySet.toString() ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+     }
+
+     System.out.println(" Starting set [6 12-sided dice]: " + theirSet.toString() );
+     for (int testVar = -1; testVar < 7; testVar++ ) {
+       System.out.print(" Roll Dice # " + testVar + ": ");
+       try { theirSet.rollIndividual( testVar ); System.out.println( theirSet.toString() ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+     }
+
+     // TEST getIndividual( int i )
+     System.out.println("\n20 TESTS FOR getIndividual( int i )");
+
+     System.out.println("Starting set [12 6-sided dice]: " + mySet.toString() );
+     for (int testVar = -1; testVar < 13; testVar++ ) {
+       System.out.print("Get Dice #" + testVar + ": ");
+       try { System.out.println( mySet.getIndividual( testVar ) ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+     }
+
+     System.out.println("Starting set [6 12-sided dice]: " + theirSet.toString() );
+     for (int testVar = -1; testVar < 7; testVar++ ) {
+       System.out.print("Get Dice #" + testVar + ": ");
+       try { System.out.println( theirSet.getIndividual( testVar ) ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+     }
+
+     // TEST toString()
+     System.out.println("\n3 TESTS FOR toString()");
+     System.out.print(" Test DiceSet with 12 6-sided dice: ");
+     try { System.out.println( mySet.toString() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     System.out.print(" Test DiceSet with 6 12-sided dice: ");
+     try { System.out.println( theirSet.toString() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     System.out.print(" Test DiceSet with 12 6-sided dice that are unrolled: ");
+     mySet = new DiceSet( 12, 6 );
+     try { System.out.println( mySet.toString() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     // TEST getSides()
+     System.out.println("\n2 TESTS for getSides()");
+     System.out.print(" Dice with 6 sides: ");
+     try { System.out.println( mySet.getSides() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     System.out.print(" Dice with 12 sides: ");
+     try { System.out.println( theirSet.getSides() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     // TEST toString( DiceSet ds )
+     System.out.println("\n3 TESTS FOR toString( DiceSet ds )");
+     System.out.print(" Test with input DiceSet with 12 6-sided dice: ");
+     try { System.out.println( mySet.toString( theirSet ) ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     System.out.print(" Test with input DiceSet with 6 12-sided dice that are unrolled: ");
+     try { System.out.println( theirSet.toString( mySet ) ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     System.out.print(" Test with input DiceSet same as executing DiceSet: ");
+     try { System.out.println( mySet.toString( mySet ) ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     // TEST isIdentical( DiceSet ds )
+     System.out.println("\n23 TESTS FOR isIdentical( DiceSet ds )");
+     System.out.print(" Test with DiceSet[12 6-sided dice] and DiceSet[6 12-sided dice]: ");
+     try { System.out.println( ( mySet.isIdentical( theirSet ) ) ? " " + mySet.toString() + " is identical to " + theirSet.toString() : " " + mySet.toString() + " is different than " + theirSet.toString() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     System.out.println(" Test DiceSet[2 4-sided dice] with  DiceSet[2 4-sided dice]: ");
+     mySet = new DiceSet( 2, 4 );
+     theirSet = new DiceSet( 2, 4 );
+
+     try { System.out.println( ( mySet.isIdentical( theirSet ) ) ? " " + mySet.toString() + " is identical to " + theirSet.toString() :" " + mySet.toString() + " is different than " + theirSet.toString() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
+     mySet.roll();
+
+     for (int i = 0; i < 20; i++ ) {
+       try { System.out.println( ( mySet.isIdentical( theirSet ) ) ? " " + mySet.toString() + " is identical to " + theirSet.toString() : " " + mySet.toString() + " is different than " + theirSet.toString() ); }
+       catch (Exception e) { System.out.println( "ERROR" ); }
+       theirSet.roll();
+     }
+
+     System.out.println("\nTest DiceSet[2 4-sided dice] with  DiceSet[2 6-sided dice]: ");
+     mySet = new DiceSet( 2, 4 );
+     theirSet = new DiceSet( 2, 6 );
+     try { System.out.println( ( mySet.isIdentical( theirSet ) ) ?" " + mySet.toString() + " is identical to " + theirSet.toString() :" " + mySet.toString() + " is different than " + theirSet.toString() ); }
+     catch (Exception e) { System.out.println( "ERROR" ); }
+
    }
 
 }
